aaf52a93542fe32fc04fb0ce3de0eb40419830ec
vector15-05 <252612603+vector15-05@users.noreply.github.com>
2026-02-26 02:10:02 +0530

    setup the next project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Home: () => React.JSX.Element = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default Home;
